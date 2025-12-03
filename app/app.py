8d69ee30844fc670b0f399de1ef8bcebf4b80e29
shaver007 <teodor.peev@gmail.com>
2025-12-03 23:53:09 +0200

    ci:Verify workflow trigger on PR and files change - verified

--- app/app.py
+++ app/app.py
@@ -4,13 +4,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
-
 if __name__ == "__main__":
     app.run(port=os.environ.get("PORT", 3000), host="0.0.0.0")
-
-#Test that lint is executed on pull request and Python files change.
